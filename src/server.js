61c0d14ffe74db850dbd2611f86d770ebab62a1f
Anhar12 <anharkhoirun@gmail.com>
2026-01-04 17:56:47 +0800

    ...

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 require("dotenv").config()
 const app = require("./app")
-const initDb = require("./config/initDB")
+const initDb = require("./config/initDb")
 
 const PORT = process.env.PORT || 5000
 
