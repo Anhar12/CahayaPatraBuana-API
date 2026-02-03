cf6bc9e5a8c17d5df8ad184a9eca57a2a5cc0338
Anhar12 <anharkhoirun@gmail.com>
2026-02-03 19:24:50 +0800

    update transaksi

--- src/routes/elpiji.routes.js
+++ src/routes/elpiji.routes.js
@@ -7,6 +7,5 @@ router.get("/", auth, elpijiController.getAll)
 router.post("/", auth, elpijiController.create)
 router.put("/:id", auth, elpijiController.update)
 router.delete("/:id", auth, elpijiController.remove)
-router.get("/export", auth, elpijiController.exportExcel)
 
 module.exports = router
